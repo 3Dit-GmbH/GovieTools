b409d91964455456c016e2e2bb8a58e804b7bbf3
Lorenz Wieseke <lorenz.wieseke@3dit.de>
2021-07-05 16:05:17 +0200

    DDITGOVCNT-371 Pfade für Mac OS anpassen

--- Operator/preview_operator.py
+++ Operator/preview_operator.py
@@ -10,25 +10,32 @@ class GOVIE_Preview_Operator(bpy.types.Operator):
     port = 8000
     url = "https://3dit-tools.s3.eu-central-1.amazonaws.com/StaticGLBViewer/index.html#model=http://127.0.0.1:"+str(port)+"/export.glb"
 
+
     @classmethod
     def poll(cls, context):
+        file_path = bpy.data.filepath
+        project_dir = os.path.dirname(file_path)
         filename = context.scene.export_settings.glb_filename
-        
-        path = bpy.path.abspath("//glb\\"+filename+".glb")
-        if os.path.exists(path):
+        glb_path = os.path.join(project_dir,'glb','')
+        glb_file = glb_path + filename + ".glb"
+
+        if os.path.exists(glb_file):
             return True
         return False
 
 
     def execute(self, context):
+        file_path = bpy.data.filepath
+        project_dir = os.path.dirname(file_path)
         filename = context.scene.export_settings.glb_filename
-        file_path = bpy.path.abspath("//glb\\"+filename+".glb")
+        glb_path = os.path.join(project_dir,'glb','')
+        glb_file = glb_path + filename + ".glb"
 
         script_file = os.path.realpath(__file__)
         script_dir = os.path.dirname(script_file)
         server_path = os.path.join(script_dir, '..',"Server\server.py")
 
-        functions.start_server(server_path,file_path,self.port)
+        functions.start_server(server_path,glb_file,self.port)
         # run browser
         bpy.ops.wm.url_open(url = self.url)
         return {"FINISHED"}
